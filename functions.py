895f431b4d3d0acfc6182021b59922815e69a748
Clarivy <zqc.sc.hn@gmail.com>
2023-08-20 22:32:22 -0700

    upd: more functions and make safer

--- functions.py
+++ functions.py
@@ -7,9 +7,24 @@ from moviepy.editor import VideoClip as _VideoClip, \
 from glob import glob as _glob
 
 
-def makeColView(video_clips: list[dict]) -> _VideoClip:
+def concatenate(video_clips: list[dict]) -> _VideoClip:
+    assert len(video_clips) > 0, "video_clips must have at least one clip"
+
     from utils import readVideoConfig as _readVideoConfig
     video_clips = list(map(_readVideoConfig, video_clips))
+
+    assert all([isinstance(clip, _VideoClip) for clip in video_clips]
+               ), "video_clips must be a list of VideoClips"
+
+    return _concatenate_videoclips(video_clips)
+
+
+def makeColView(video_clips: list[dict]) -> _VideoClip:
+    assert len(video_clips) > 0, "video_clips must have at least one clip"
+
+    from utils import readVideoConfig as _readVideoConfig
+    video_clips = list(map(_readVideoConfig, video_clips))
+
     max_duration = max([clip.duration for clip in video_clips])
     padded_clips = [_adjust_clip_duration(
         clip, max_duration) for clip in video_clips]
@@ -19,13 +34,15 @@ def makeColView(video_clips: list[dict]) -> _VideoClip:
     return final_clip
 
 
-def makeGridView(video_clips_2d: list[list[dict]]) -> _VideoClip:
+def makeGrid2D(video_clips_2d: list[list[dict]]) -> _VideoClip:
     assert len(video_clips_2d) > 0, "video_clips_2d must have at least one row"
-    assert len(video_clips_2d[0]) > 0, "video_clips_2d must have at least one column"
-    assert all([len(row) == len(video_clips_2d[0]) for row in video_clips_2d]), "video_clips_2d must have the same number of columns in each row"
-    
+    assert len(
+        video_clips_2d[0]) > 0, "video_clips_2d must have at least one column"
+    assert all([len(row) == len(video_clips_2d[0]) for row in video_clips_2d]
+               ), "video_clips_2d must have the same number of columns in each row"
+
     from utils import readVideoConfig as _readVideoConfig
-    video_clips_2d = [[clip if isinstance(clip, _VideoClip) else _readVideoConfig(clip)
+    video_clips_2d = [[_readVideoConfig(clip)
                        for clip in row] for row in video_clips_2d]
     max_duration = max(
         [clip.duration for row in video_clips_2d for clip in row])
@@ -37,22 +54,23 @@ def makeGridView(video_clips_2d: list[list[dict]]) -> _VideoClip:
     return final_clip
 
 
-def makeGrid(*, video_clips, grid_size: tuple[int, int]) -> _VideoClip:
-    from utils import readVideoConfig as _readVideoConfig
-    if isinstance(video_clips, dict):
-        video_clips = _readVideoConfig(video_clips)
-
-    assert len(video_clips) == grid_size[0] * \
-        grid_size[1], "video_clips must have the same number of clips as the grid size"
+def makeGrid1D(*, video_clips, grid_size: tuple[int, int]) -> _VideoClip:
     assert len(video_clips) > 0, "video_clips must have at least one clip"
     assert grid_size[0] > 0 and grid_size[1] > 0, "grid_size must be positive"
+
+    from utils import readVideoConfig as _readVideoConfig
+    video_clips = _readVideoConfig(video_clips)
+
+    assert len(video_clips) == grid_size[0] * \
+        grid_size[1], f"video_clips({len(video_clips)}) must have the same number of clips as the grid size({grid_size[0] * grid_size[1]})"
+
     video_clips_2d = []
     for i in range(grid_size[0]):
         row = []
         for j in range(grid_size[1]):
             row.append(video_clips[i * grid_size[1] + j])
         video_clips_2d.append(row)
-    return makeGridView(video_clips_2d)
+    return makeGrid2D(video_clips_2d)
 
 
 def _adjust_clip_duration(clip: _VideoClip, max_duration: float) -> _VideoClip:
@@ -64,13 +82,23 @@ def _adjust_clip_duration(clip: _VideoClip, max_duration: float) -> _VideoClip:
         return clip
 
 
-def readVideoFile(video_files: list[str]) -> _VideoClip:
-    return _VideoFileClip(video_files, fps_source='fps')
+def readVideoFile(video_file: str) -> _VideoClip:
+    assert isinstance(video_file, str), "video_file must be a string"
+
+    return _VideoFileClip(video_file, fps_source='fps')
 
 
 def readVideoFiles(video_files: list[str]) -> list[_VideoClip]:
+    assert len(video_files) > 0, "video_files must have at least one file"
+
     return list(map(readVideoFile, video_files))
 
 
 def readVideoFilePattern(video_file_pattern: str) -> _VideoClip:
+    assert isinstance(video_file_pattern,
+                      str), "video_file_pattern must be a string"
+
     return readVideoFiles(_glob(video_file_pattern))
+
+def useBlackVideo(video_size: tuple[int, int], duration: float) -> _VideoClip:
+    return _ColorClip(video_size, col=(0, 0, 0), duration=duration)
